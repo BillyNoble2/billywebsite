cd4ae9746ca63f3438b0c8c2b98809ca56e818d5
Billy Noble <billyjnoble@outlook.com>
2024-02-12 17:01:33 +0000

    About me section created.

---
+++ src/NavBar.js
@@ -0,0 +1,24 @@
+import React from "react";
+import './NavBar.css';
+
+function AButton({someText}) {
+    return(
+    <button>{someText}</button>
+    );
+};
+
+export default function NavBar () {
+    return(
+        <div>
+            <div className = "Heading">
+                <h1>Billy Noble</h1>
+                <h2>Graduate Software Developer</h2>
+            </div>
+            <div className="NavBar">
+                <AButton someText={"About Me"}/>
+                <AButton someText={"Projects"}/>
+                <AButton someText={"Contact Me"}/>
+            </div>
+        </div>
+    );
+};
